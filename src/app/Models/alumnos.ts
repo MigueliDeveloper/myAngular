2e3dadf9a0aa82df82c9f2840787bd1addd1c48a
MigueliDeveloper <miguelidevelop@gmail.com>
2023-08-22 16:18:56 +0100

    holit6

---
+++ src/app/Models/alumnos.ts
@@ -0,0 +1,24 @@
+export var profesorBase = {
+    nombre: "Manuel",
+    apellidos: "M.P.",
+    correo: "manuelmacpe@gmail.com",
+    foto:"./assets/media/fotommp.jpg"
+}
+
+export class alumnosBD{
+
+    constructor(
+        public nombreA: String,
+        public apellidoA: String,
+        public edadA: number,
+        public correeoA: String,
+        public telefonoA: String,
+        public fotoA: String,
+        public estadoA: boolean,
+        
+        ){}
+
+
+
+
+}
--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { profesorBase } from '../Models/alumnos';
+import { alumnosBD } from '../Models/alumnos';
+
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
-export class CursosComponent {
 
+export class CursosComponent implements OnInit{
+
+  public alumnosAll: Array<alumnosBD>
+  public nombreProfesor: string
+  public fotoProfesor: string
   public nombre: string // Manuel
   public descripcion: string
   public listado: string // Manuel
@@ -16,19 +23,26 @@ export class CursosComponent {
 
 
   constructor(){
+    this.nombreProfesor = profesorBase.nombre
+    this.fotoProfesor = profesorBase.foto
     this.nombre="Mi curso de Programación"
     this.descripcion= "Aprende a programar desde cero"
     this.listado= "Listado de Cursos"
     this.temario= ["Introducción a la programación", "Variables y tipos de datos", "Estructuras de control"]
     this.progreso= 0  
     this.imagen= "https://thecatapi.com/api/images/get?format=src&type=gif?results_per_page"
+    this.alumnosAll=[
+      new alumnosBD("Miguel", "Ramírez", 39, "miguel@gmail.com", "1234567", "./assets/media/fotommp.jpg", true),
+      
+      new alumnosBD("Javier", "Ramírez", 19, "javier@gmail.com", "456789", "./assets/media/fotommp.jpg", true)
+    ]
+
+  
   }
   
   ngOnInit(){
     console.log("OnInit Ejecutado")
 
-
-    
   }
 
   cambiarTitulo(){
