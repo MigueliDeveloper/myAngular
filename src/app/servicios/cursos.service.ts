a0c02f30602ee03020409629a09c17ad8afc7b6d
miguelidevelop <miguelidevelop@gmail.com>
2023-08-28 18:25:28 +0100

    holi

--- src/app/servicios/cursos.service.ts
+++ src/app/servicios/cursos.service.ts
@@ -1,2 +1,34 @@
 import { Injectable } from "@angular/core";
 import { cursosBD } from "../Models/cursos";
+
+@Injectable({
+  provideIn: 'root'
+})
+
+export class CursosService {
+  private cursosall: Array<cursosBD>
+
+    constructor(){
+
+      this.cursosall = [
+        new cursosBD("Matematicas", "Asignatura de Matematicas", 580),
+        new cursosBD("Lógica de Programación", "Asignatura de Logica de Programación", 650),
+        new cursosBD("Física", "Asignatura de Física", 500),
+      ]
+
+    }
+
+    getTexto(){
+      return "Texto desde un servicio"
+
+    }
+
+    getCursos(): Array<cursosBD>{
+
+        return this.cursosall
+    }
+
+    setCursos(){
+    }
+
+}
