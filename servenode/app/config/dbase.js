c3d336fe0fe105d63d1a6d0d506af732628faabf
MigueliDeveloper <miguelidevelop@gmail.com>
2023-09-21 17:38:53 +0100

    holi

--- servenode/app/config/dbase.js
+++ servenode/app/config/dbase.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: 'variable.env'});
 
 const conectarDB = async() => {
     try{
-
+        
         await mongoose.connect(process.env.DB_MONGO,{
             // useNewUrlParser: true,
             // useUnifiedTopoly: true
